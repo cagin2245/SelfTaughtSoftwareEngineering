6c77c170af9d73b2192ae1e0f86e4580c0baa606
cagin kilinc <caginkilincyt@gmail.com>
2025-08-24 04:49:31 +0300

    Refactor MyVector class: improve member variable naming, add erase and insert methods, and enhance memory management

--- includes/my_vector.hpp
+++ includes/my_vector.hpp
@@ -1,20 +1,20 @@
 #pragma once // my_vector.hpp
 
 #include <iostream>
+#include <memory>
 
-using namespace std;
 
 template <typename T>
 class MyVector
 {
 private:
-    T *data;
-    size_t capacity;
-    size_t size;
+    T *data_;
+    size_t capacity_;
+    size_t size_;
 
 public:
     MyVector();
-    MyVector(size_t n, const T &value);
+    MyVector(std::size_t n, const T &value);
     ~MyVector();
 
     MyVector(const MyVector &other);                // copy constructor
@@ -24,22 +24,28 @@ public:
 
     void push_back(const T &value);
     void pop_back();
-    T &operator[](size_t index);
-    const T &operator[](size_t index) const;
+    void erase(std::size_t index); // Remove element at index
+    T &operator[](std::size_t index);
+    const T &operator[](std::size_t index) const;
+    const T &at(std::size_t index) const;
+    T &at(std::size_t index);
 
-    size_t getSize() const;
-    size_t getCapacity() const;
+    std::size_t getSize() const;
+    std::size_t getCapacity() const;
     bool isEmpty() const;
 
-    void reserve(size_t newCapacity);
-    void resize(size_t newSize, const T &value = T());
-    void swap(MyVector &other)
-    {
-        std::swap(data, other.data);
-        std::swap(capacity, other.capacity);
-        std::swap(size, other.size);
-    }
-    // Iterators
+    T &front();
+    const T &front() const;
+    T &back();
+    const T &back() const;
+    void clear(); // Clear the vector by resetting size to 0
+    void reserve(std::size_t newCapacity);
+    void resize(std::size_t newSize, const T &value = T());
+    void insert(std::size_t index, const T &value);
+    void swap(MyVector &other);
+
+    void shrink_to_fit();
+
     class Iterator
     {
     private:
@@ -55,6 +61,7 @@ public:
         }
         bool operator!=(const Iterator &other) const { return ptr != other.ptr; }
     };
+
     class ConstIterator
     {
     private:
@@ -70,149 +77,293 @@ public:
         }
         bool operator!=(const ConstIterator &other) const { return ptr != other.ptr; }
     };
-    
-    T *begin() { return data; }
-    T *end() { return data + size; }
-    const T *begin() const { return data; }
-    const T *end() const { return data + size; }
+
+    Iterator begin() { return Iterator(data_); }
+    Iterator end() { return Iterator(data_ + size_); }
+    ConstIterator begin() const { return ConstIterator(data_); }
+    ConstIterator end() const { return ConstIterator(data_ + size_); }
 };
 
+// Implementation
+
 template <typename T>
-void MyVector<T>::reserve(size_t newCapacity)
+MyVector<T>::MyVector() : data_(nullptr), capacity_(0), size_(0) {}
+
+template <typename T>
+MyVector<T>::MyVector(std::size_t n, const T &value) : data_(nullptr), capacity_(n), size_(n)
 {
-    if (newCapacity > capacity)
+    data_ = new T[capacity_];
+    for (size_t i = 0; i < size_; ++i)
     {
-        T *newData = new T[newCapacity];
-        for (size_t i = 0; i < size; ++i)
-        {
-            newData[i] = std::move(data[i]);
-        }
-        delete[] data;
-        data = newData;
-        capacity = newCapacity;
+        data_[i] = value;
     }
 }
 
-template <typename T>
-void MyVector<T>::resize(size_t newSize, const T &value)
-{
-    if (newSize < size)
-    {
-        // Küçültme → sadece size değişiyor
-        size = newSize;
-    }
-    else if (newSize > size)
-    {
-        if (newSize > capacity)
-        {
-            reserve(newSize);
-        }
-        for (size_t i = size; i < newSize; ++i)
-        {
-            data[i] = value;
-        }
-        size = newSize;
-    }
-}
-
-template <typename T>
-size_t MyVector<T>::getSize() const
-{
-    return size;
-}
-template <typename T>
-size_t MyVector<T>::getCapacity() const
-{
-    return capacity;
-}
-template <typename T>
-bool MyVector<T>::isEmpty() const
-{
-    return size == 0;
-}
-
-template <typename T>
-MyVector<T>::MyVector() : data(nullptr), capacity(0), size(0) {} // Default constructor initializes an empty vector
-template <typename T>
-MyVector<T>::MyVector(size_t n, const T &value) : capacity(n), size(n)
-{
-    data = new T[capacity];
-    for (size_t i = 0; i < size; ++i)
-    {
-        data[i] = value; // Initialize with the given value
-    }
-}
 template <typename T>
 MyVector<T>::~MyVector()
 {
-    delete[] data; // Destructor to free allocated memory
+    delete[] data_;
 }
+
 template <typename T>
-MyVector<T>::MyVector(const MyVector &other) : capacity(other.capacity), size(other.size)
+MyVector<T>::MyVector(const MyVector &other) : data_(nullptr), capacity_(other.capacity_), size_(other.size_)
 {
-    data = new T[capacity];
-    for (size_t i = 0; i < size; ++i)
+    data_ = new T[capacity_];
+    for (size_t i = 0; i < size_; ++i)
     {
-        data[i] = other.data[i]; // Copy elements from the other vector
+        data_[i] = other.data_[i];
     }
 }
 
 template <typename T>
-MyVector<T>::MyVector(MyVector &&other) noexcept : data(other.data), capacity(other.capacity), size(other.size)
+MyVector<T>::MyVector(MyVector &&other) noexcept : data_(other.data_), capacity_(other.capacity_), size_(other.size_)
 {
-    other.data = nullptr; // Leave the moved-from object in a valid state
-    other.capacity = 0;
-    other.size = 0;
+    other.data_ = nullptr;
+    other.capacity_ = 0;
+    other.size_ = 0;
 }
+
 template <typename T>
 MyVector<T> &MyVector<T>::operator=(const MyVector &other)
 {
     if (this != &other)
     {
-        MyVector temp(other); // copy constructor çağrılır
-        swap(temp);           // elimizdeki ile temp değişir
+        MyVector temp(other);
+        swap(temp);
     }
     return *this;
 }
+
 template <typename T>
 MyVector<T> &MyVector<T>::operator=(MyVector &&other) noexcept
 {
     if (this != &other)
     {
-        delete[] data;
-        data = other.data;
-        capacity = other.capacity;
-        size = other.size;
-        other.data = nullptr;
-        other.capacity = 0;
-        other.size = 0;
+        std::swap(data_, other.data_);
+        std::swap(size_, other.size_);
+        std::swap(capacity_, other.capacity_);
     }
     return *this;
 }
+
 template <typename T>
 void MyVector<T>::push_back(const T &value)
 {
-    if (size >= capacity)
+    if (size_ >= capacity_)
     {
-        reserve(capacity == 0 ? 1 : capacity * 2); // Double the capacity if needed
+        reserve(capacity_ == 0 ? 1 : capacity_ * 2);
     }
-    data[size++] = value; // Add the new element and increment size
+    data_[size_++] = value;
 }
+
 template <typename T>
 void MyVector<T>::pop_back()
 {
-    if (size > 0)
+    if (size_ > 0)
     {
-        data[size - 1].~T(); // Call destructor for the last element (primitive types don't need this but it's safe)
-        --size;              // Decrement size to remove the last element
+        data_[size_ - 1].~T();
+        --size_;
     }
 }
+
 template <typename T>
-T &MyVector<T>::operator[](size_t index)
+void MyVector<T>::erase(std::size_t index)
 {
-    if (index >= size)
+    if (index >= size_)
     {
         throw std::out_of_range("Index out of range");
     }
-    return data[index]; // Return the element at the specified index
+    for (size_t i = index; i < size_ - 1; ++i)
+    {
+        data_[i] = std::move(data_[i + 1]);
+    }
+    data_[size_ - 1].~T();
+    --size_;
+}
+
+template <typename T>
+T &MyVector<T>::operator[](std::size_t index)
+{
+    if (index >= size_)
+    {
+        throw std::out_of_range("Index out of range");
+    }
+    return data_[index];
+}
+
+template <typename T>
+const T &MyVector<T>::operator[](std::size_t index) const
+{
+    if (index >= size_)
+    {
+        throw std::out_of_range("Index out of range");
+    }
+    return data_[index];
+}
+
+template <typename T>
+T &MyVector<T>::at(std::size_t index)
+{
+    if (index >= size_)
+        throw std::out_of_range("Index out of range");
+    return data_[index];
+}
+
+template <typename T>
+const T &MyVector<T>::at(std::size_t index) const
+{
+    if (index >= size_)
+        throw std::out_of_range("Index out of range");
+    return data_[index];
+}
+
+template <typename T>
+std::size_t MyVector<T>::getSize() const
+{
+    return size_;
+}
+
+template <typename T>
+std::size_t MyVector<T>::getCapacity() const
+{
+    return capacity_;
+}
+
+template <typename T>
+bool MyVector<T>::isEmpty() const
+{
+    return size_ == 0;
+}
+
+template <typename T>
+T &MyVector<T>::front()
+{
+    if (isEmpty())
+    {
+        throw std::out_of_range("Vector is empty");
+    }
+    return data_[0];
+}
+
+template <typename T>
+const T &MyVector<T>::front() const
+{
+    if (isEmpty())
+    {
+        throw std::out_of_range("Vector is empty");
+    }
+    return data_[0];
+}
+
+template <typename T>
+T &MyVector<T>::back()
+{
+    if (isEmpty())
+    {
+        throw std::out_of_range("Vector is empty");
+    }
+    return data_[size_ - 1];
+}
+
+template <typename T>
+const T &MyVector<T>::back() const
+{
+    if (isEmpty())
+    {
+        throw std::out_of_range("Vector is empty");
+    }
+    return data_[size_ - 1];
+}
+
+template <typename T>
+void MyVector<T>::clear()
+{
+    for (size_t i = 0; i < size_; ++i)
+    {
+        data_[i].~T();
+    }
+    size_ = 0;
+}
+
+template <typename T>
+void MyVector<T>::reserve(std::size_t newCapacity)
+{
+    if (newCapacity <= capacity_)
+        return;
+    T *newData = new T[newCapacity];
+    for (size_t i = 0; i < size_; ++i)
+    {
+        newData[i] = std::move(data_[i]);
+    }
+    delete[] data_;
+    data_ = newData;
+    capacity_ = newCapacity;
+}
+
+template <typename T>
+void MyVector<T>::resize(std::size_t newSize, const T &value)
+{
+    if (newSize < size_)
+    {
+        for (std::size_t i = newSize; i < size_; ++i)
+        {
+            data_[i].~T();
+        }
+        size_ = newSize;
+    }
+    else if (newSize > size_)
+    {
+        if (newSize > capacity_)
+        {
+            reserve(newSize);
+        }
+        for (std::size_t i = size_; i < newSize; ++i)
+        {
+            data_[i] = value;
+        }
+        size_ = newSize;
+    }
+}
+
+template <typename T>
+void MyVector<T>::insert(std::size_t index, const T &value)
+{
+    if (index > size_)
+    {
+        throw std::out_of_range("Index out of range");
+    }
+    if (size_ >= capacity_)
+    {
+        reserve(capacity_ == 0 ? 1 : capacity_ * 2);
+    }
+    for (size_t i = size_; i > index; --i)
+    {
+        data_[i] = std::move(data_[i - 1]);
+    }
+    data_[index] = value;
+    ++size_;
+}
+
+template <typename T>
+void MyVector<T>::swap(MyVector &other)
+{
+    std::swap(data_, other.data_);
+    std::swap(size_, other.size_);
+    std::swap(capacity_, other.capacity_);
+}
+
+template <typename T>
+void MyVector<T>::shrink_to_fit()
+{
+    if (size_ < capacity_)
+    {
+        T *newData = new T[size_];
+        for (size_t i = 0; i < size_; ++i)
+        {
+            newData[i] = std::move(data_[i]);
+        }
+        delete[] data_;
+        data_ = newData;
+        capacity_ = size_;
+    }
 }
